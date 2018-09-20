12b8ee7094026d8bf967e5d338a8d7084acb5ae0
emilx03 <40799067+emilx03@users.noreply.github.com>
2018-09-20 22:52:46 +0200

    Erstellen der HauptFunktion

---
+++ animation.js
@@ -0,0 +1,71 @@
+var bird;
+var t;
+var eingabe;
+var stop = false;
+
+window.onload = function(){
+
+  /*wenn Leertaste eingedrückt ist*/
+  window.addEventListener('keydown', function(event){
+    /*sorgt dafür, dass im Zusammenhand mit keyup die Prozedur
+    "bird.up()" (welche den Bird zum Fliegen bringt), nur einmal
+    aufgerufen wird, wenn die Leertaste gedrückt wurde*/
+    if(stop == false){
+      //event.which gibt den Ascii der gedrpckten Taste aus
+      //32 ist also die Leertaste
+      if(event.which == 32){
+        stop = true;
+        bird.up();
+      }
+    }
+  });
+
+  /*wenn Leertaste wieder losgelassen wird*/
+  window.addEventListener('keyup', function(event){
+    //event.which gibt den Ascii der gedrpckten Taste aus
+    //32 ist also die Leertaste
+    if(event.which == 32){
+      stop = false;
+    }
+  });
+
+  bird = new bird;
+  /*eigentlich nur ein extra, damit der Bird sofort bei Aufruf
+  der Seite in der Startposition ist*/
+  bird.show();
+  /*alle 10 millisekunden: +Funktion "update()"
+  aufgerufen, welche sich um die Grvitation durch die
+  Funktion "bird.update()" kümmert" +if-Abfragen werden
+  aufgerufen, welche dafür sorgen dass der Bird das
+  Spielfeld nicht verlässt +bird.coords wird auch immer
+  wieder aufgerufen, und sorgt in diesemFall dafür, dass
+  wenn der Bird das Spielfeld verlässt, er auf die
+  richtige Position gesetzt wird.*/
+  t = setInterval(function update(){
+    /*bird.update()-->
+    bie jedem Aufruf verändern sich die Koordinaten
+    vom bird. Darf also nicht ans ende dieser Funktion,
+    da, wenn er das Spielfeld verlassen würde, nochmal
+    an Höhe verlieren würde*/
+    bird.update();
+    /*solange die yKoordinate vom bird größer als der Wert
+    der Spielfläche ist, wird der bird immer auf den
+    unteren Bildrand des Spielfelds gesetzt*/
+    if(bird.y >= 570){
+      bird.velocity = 0;
+      bird.y = 570;
+      /*der bird kann nicht höher als die Spielfläche fliegen.
+      Berührt er also die "decke" fällt er von dort wieder
+      herunter*/
+    }else if(bird.y <= 0){
+      bird.velocity = 0;
+      bird.y = 0;
+    }
+    /*am Ende wird nochmal bird.coords aufgerufen, da wenn
+    der Bird das Spielfeld verlässt, der Bird nicht erst im
+    nächsten Interval auf die maximalen und minimalen
+    Werte positioniert wird.*/
+    bird.coords();
+  }, 20);
+
+}
